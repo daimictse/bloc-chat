cc98c7f457a0047d7d5af39c149a62c79d8878a8
daimictse <daimictse@gmail.com>
2018-07-25 22:54:14 -0700

    chat-list-messages completed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -6,25 +6,26 @@ class MessageList extends Component {
     this.state = {
       messages: []
     };
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.messagesRef = this.props.firebase.database().ref('messages');
   }
 
   componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
-      const room = snapshot.val();
-      if (room.messages) {
-          //console.log("ML msg:", room.messages.m1);
-        this.setState({messages: this.state.messages.concat( room.messages )});
-      }
-      console.log("ML", this.state.messages);
+    this.messagesRef.on('child_added', snapshot => {
+      const message = snapshot.val();
+      message.key = snapshot.key;
+      this.setState({messages: this.state.messages.concat( message )});
     });
-    /*
-    this.roomsRef.orderByChild("name").equalTo(this.props.activeRoom.key).on('child_added', snapshot => {
-      const room = snapshot.val();
-      if (room.messages)
-        this.setState({messages: this.state.messages.concat( room.messages )});
-      console.log("ML", this.state.messages);
-    });*/
+  }
+
+  formatTime(unixTime)
+  {
+    var dt = new Date(unixTime*1000);
+    var hr = dt.getHours();
+    var apm = hr < 12 ? "am" : "pm";
+    hr = hr % 12;
+    hr = hr === 0 ? 12 : hr;
+    var m = "0" + dt.getMinutes();
+    return hr + ':' + m.substr(-2) + " " + apm;
   }
 
   render() {
@@ -32,8 +33,18 @@ class MessageList extends Component {
       <section className="messages">
         <div className='room-title'>{this.props.activeRoom.name}</div>
         <div className="conversation">
-          { /* filter message here? */
-          }
+        {
+          this.state.messages.filter( (message, index ) =>
+            message.roomId === this.props.activeRoom.key
+          ).map( (message, index ) =>
+            <div className="message" key={index}>
+              <div className="message-user">{message.username}
+                <span className="message-sentAt">{this.formatTime(message.sentAt)}</span>
+              </div>
+              <div className="message-content">{message.content}</div>
+            </div>
+          )
+        }
           </div>
       </section>
     );
